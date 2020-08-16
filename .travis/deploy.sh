dd3ed24ad4022e947c0d565d732b578f5b83d727
Dom Masters <dom@processcreative.com.au>
2020-08-17 08:38:28 +1000

    Removed version (testing)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,9 +6,9 @@ git config core.filemode false
 git remote set-url origin https://${GH_TOKEN}@github.com/${OWNER}/slate-v2.git
 git fetch --all
 git checkout master
-yarn lerna version patch --conventional-commits --yes --exact
-git push --follow-tags
-git checkout master
+# yarn lerna version patch --conventional-commits --yes --exact
+# git push --follow-tags
+# git checkout master
 
 for D in `find ./packages/ -mindepth 1 -maxdepth 1 -type d`
 do
