0943692d4522c6c96799ba276b398d119800f0cc
Dominic Masters <dominic@domsplace.com>
2020-10-02 08:25:47 +1000

    Cleaned up slate-theme

--- packages/slate-theme/src/scripts/layout/theme.ts
+++ packages/slate-theme/src/scripts/layout/theme.ts
@@ -1,4 +1,19 @@
-'use strict';
+import './../base';
 
 //Import Theme Styles
 import '../../styles/layout/theme.scss';
+
+//Theme Objects
+
+//Theme Components
+
+//Theme Tools
+if(process.env.NODE_ENV === 'development') {
+  const SOME_VAR = 'SOME_VALUE';
+  //Dev Tools
+  require('../tools/layout/grid-view/GridView');
+}
+
+// Let CSS know JS is available
+document.documentElement.classList.remove('js-unavailable');
+document.documentElement.classList.add('js-available');
