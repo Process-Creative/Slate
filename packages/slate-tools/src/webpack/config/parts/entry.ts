d6f84f12759fb52576b52244e3930ef38288f990
Dominic Masters <dominic@domsplace.com>
2021-02-10 19:14:12 +1100

    Fixed jest not unit testing properly.

--- packages/slate-tools/src/webpack/config/parts/entry.ts
+++ packages/slate-tools/src/webpack/config/parts/entry.ts
@@ -3,10 +3,9 @@ import { getLayoutEntryPoints } from '../utilities/get-layout-entrypoints';
 import { getTemplateEntryPoints } from '../utilities/get-template-entrypoints';
 
 export const partEntry = {
-  entry: Object.assign(
-    {},
-    getLayoutEntryPoints(),
-    getTemplateEntryPoints(),
-    slateToolsConfig.get('webpack.entrypoints'),
-  ),
+  entry: {
+    ...getLayoutEntryPoints(),
+    ...getTemplateEntryPoints(),
+    ...(slateToolsConfig.get('webpack.entrypoints') || {}),
+  }
 };
