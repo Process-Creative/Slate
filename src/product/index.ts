370e06906e49c64acd997fc1a37f9495f68256d9
Dominic Masters <dominic@domsplace.com>
2021-12-30 11:33:30 -0800

    Corrected exports for product

--- src/product/index.ts
+++ src/product/index.ts
@@ -1,4 +1,6 @@
 export * from './ajax';
-export * from './recommendations';
+export * from './collection';
 export * from './product';
+export * from './recently-viewed';
 export * from './variant';
+export * from './recommendations';
