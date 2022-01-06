4d5e4cf628df9c84d34bb07b5ac25de10bd61d39
Dominic Masters <dominic@domsplace.com>
2022-01-05 17:12:35 -0800

    Corrected invalid window.Customer fetcher.

--- src/customer/index.ts
+++ src/customer/index.ts
@@ -1,5 +1,7 @@
-export const customerGet = () => {
-  if(!window || !window.Customer) {
+import { Customer } from "../types/customer";
+
+export const customerGet = ():Customer|null => {
+  if(!window || typeof window.Customer === typeof undefined) {
     throw new Error('Customer is undefined... probably because you haven\'t set window.Customer to a JSON formatted Customer object!');
   }
   return window.Customer;
