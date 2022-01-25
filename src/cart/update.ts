c646eeca70f20e94f2355e9d8aa2159fd3137899
Dominic Masters <dominic@domsplace.com>
2022-01-25 14:21:53 -0800

    Fixed cart update URL

--- src/cart/update.ts
+++ src/cart/update.ts
@@ -22,7 +22,7 @@ class EventCartUpdated extends CustomEvent<{ cart:Cart }> {
 
 export const cartUpdate = (update:CartUpdate) => cartQueue((async () => {
   try {
-    const response:Cart = await fetch('/cart/changeupdate.js', {
+    const response:Cart = await fetch('/cart/update.js', {
       method: 'POST',
       body: JSON.stringify(update),
       headers: { 'Content-Type': 'application/json' }
