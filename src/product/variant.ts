98eca723b5108add8784a2db15d8513a2b6ae75c
Dominic Masters <dominic@domsplace.com>
2022-02-09 10:24:51 -0800

    Added variantSelectedOrFirstAvailable

--- src/product/variant.ts
+++ src/product/variant.ts
@@ -1,6 +1,6 @@
 import { productGetUrl } from "./product"
 import { handlize } from "../string";
-import { Maybe, WithCollection, WithOptions, WithProduct, WithVariant, WithVariants } from "../types";
+import { Maybe, Variant, WithCollection, WithOptions, WithProduct, WithVariant, WithVariants } from "../types";
 
 export const variantFirstAvailable = ({ variants }:WithVariants) => {
   return variants.find(v => v.available);
@@ -8,6 +8,31 @@ export const variantFirstAvailable = ({ variants }:WithVariants) => {
 
 export const variantFirstAvailableOrDefault = (p:WithVariants) => {
   return variantFirstAvailable(p) || p.variants[0];
+}
+
+/** Params used to select a variant */
+export type VariantSelectedParams = { selected?:number|Variant };
+
+/**
+ * Returns the currently selected variant. If no or an invalid variant is 
+ * selected then the first available or default variant will be returned.
+ * 
+ * @param p Array of variants, as well as the selected variant.
+ * @returns The selected variant, first available variant, or default variant.
+ */
+export const variantSelectedOrFirstAvailable = (p:(
+  WithVariants & VariantSelectedParams
+)):Variant => {
+  let variant:Variant|null|undefined = null;
+  if(p.selected) {
+    if((p.selected as Variant).id) {
+      variant = p.variants.find(v => v.id === (p.selected as Variant).id);
+    } else {
+      variant = p.variants.find(v => v.id === p.selected as number);
+    }
+  }
+  if(variant) return variant;
+  return variantFirstAvailableOrDefault(p);
 }
 
 export const variantGetFromOptions = (params:WithOptions & WithProduct) => (
