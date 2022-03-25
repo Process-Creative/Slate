ec8b188f7e08beb8575075e0846caf4d29fe18ce
Dominic Masters <dominic@domsplace.com>
2022-03-25 16:27:32 -0700

    Extending the jQueryPolyfill type

--- src/support/index.ts
+++ src/support/index.ts
@@ -1,5 +1,12 @@
 type jQueryPolyfill = (selector:any) => {
   trigger: (method:string, params?:any) => void;
+  length:number;
+  find:jQueryPolyfill;
+  text:(str?:string)=>string;
+  on:(...props:any[])=>any;
+  attr:(s?:string)=>string;
+  toggleClass:(str:string)=>void;
+  click:()=>void;
 };
 
 
